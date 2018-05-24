2c2767504766d104494e6f2b1bf1e53eb47e03fc
Zoe <zoereagan@gmail.com>
2018-05-24 15:53:10 -0500

    created response model and started response table

--- db/migrations.sql
+++ db/migrations.sql
@@ -6,3 +6,18 @@ CREATE TABLE users(
   last_name VARCHAR(64),
   birth_date DATE,
 );
+
+
+CREATE TABLE responses(
+  id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES users(id),
+  response_1 TEXT,
+  response_2 TEXT,
+  response_3 TEXT,
+  response_4 TEXT,
+  response_5 TEXT,
+  response_6 TEXT,
+  response_7 TEXT,
+  response_8 TEXT,
+  response_9 TEXT
+)
